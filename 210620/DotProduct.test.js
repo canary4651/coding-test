9060d34c98c1352ea40c8cd31d2c478163261aa4
YE H <canary4651@gmail.com>
2021-06-20 23:12:03 +0900

    Solve dot product problem
    - [내적](https://programmers.co.kr/learn/courses/30/lessons/70128)

---
+++ 210620/DotProduct.test.js
@@ -0,0 +1,19 @@
+function solution1(a,b) {
+  let result = 0;
+
+  for (let i in a) {
+      result += a[i] * b[i]
+    }
+    return result;
+}
+
+function solution2(a, b) {
+  return a.reduce((acc, cur, i) => acc + cur * b[i], 0);
+}
+
+test('solution', () => {
+  [solution1, solution2].forEach((solution) => {
+    expect(solution([1,2,3,4], [-3,-1,0,2])).toBe(3);
+    expect(solution([-1,0,1], [1,0,-1])).toBe(-2);
+  })
+});
