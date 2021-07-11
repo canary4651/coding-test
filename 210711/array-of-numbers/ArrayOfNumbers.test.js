9e8ac27ef4bab0a51253408a8d32ade4dcaf7b39
YE H <canary4651@gmail.com>
2021-07-11 23:28:21 +0900

    Solve array of numbers problem
    - [나누어 떨어지는 숫자 배열](https://programmers.co.kr/learn/courses/30/lessons/12910)

---
+++ 210711/array-of-numbers/ArrayOfNumbers.test.js
@@ -0,0 +1,17 @@
+function solution(arr, divisor) {
+  let result = [];
+
+  arr.map((v) => {
+    if (v % divisor === 0) {
+      result.push(v);
+    } 
+  })
+  
+  return result.length ? result.sort((a,b) => a - b) : [-1];
+}
+
+test('solution', () => {
+  expect(solution([5,9,7,10], 5)).toStrictEqual([5,10]);
+  expect(solution([2, 36, 1, 3], 1)).toStrictEqual([1, 2, 3, 36]);
+  expect(solution([3,2,6], 10)).toStrictEqual([-1]);
+});
