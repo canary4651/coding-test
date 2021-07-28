64ebd34382472252a0eb078d0ab64dda34c4206a
YE H <canary4651@gmail.com>
2021-07-28 23:11:51 +0900

    Solve number of 124 world level2 problem
    - [124나라의 숫자](https://programmers.co.kr/learn/courses/30/lessons/12899)

---
+++ 210728/word-of-124/WorldOf124.test.js
@@ -0,0 +1,13 @@
+const div = (a, b) => parseInt(a / b);
+
+const step = (n) => n < 3 ? '124'[n] : step(div(n, 3) - 1) + step(n % 3);
+
+function solution(n) {
+return step(n - 1);
+}   
+
+test('solution', () => {
+  expect(solution(1)).toBe('1');
+  expect(solution(3)).toBe('4');
+  expect(solution(4)).toBe('11');
+});
